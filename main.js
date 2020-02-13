eb809d4813f4397b612a274805d25886ab1486e1
Jeffrey Lau <jkwok.lau@gmail.com>
2020-02-12 16:16:59 -0800

    added documentation to js file

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const s = "JavaScript";
-console.log(s)
+/**
+ * Function for converting decimal numbers to hexidecimal.
+ * @param {string} number
+ *
+ **/
+
+deciToHexi = (number) => {
+    return number.toString(16).toUpperCase();
+}
+console.log(deciToHexi(123));
+
